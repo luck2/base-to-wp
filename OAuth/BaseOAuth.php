31bb207761a59d34034c97ba6c986635a6deee7b
K.Sasaki <among753@gmail.com>
2014-05-21 09:55:48 +0900

    edit (+6 squashed commits) Squashed commits: [576cdc1] Edit: BaseOAuth [d91c89c] Finish: install page [7edb1bd] Edit: secret key [cafb03f] Edit: admin install [ecdc70a] Edit install [7a15158] プラグイン化

---
+++ OAuth/BaseOAuth.php
@@ -0,0 +1,303 @@
+<?php
+/**
+ * BaseOAuth
+ *
+ * BASE API v1
+ * https://gist.github.com/baseinc/9634675
+ *
+ * 設計志向
+ * 必要なコンフィグをつけてクラス化します
+ * request()で柔軟にAPIとやりとりできます
+ * あとはAPIの数だけエンドポイントを用意します
+ *
+ * @author K.Sasaki
+ * @version 0.1.0
+ */
+
+namespace OAuth;
+
+if (!class_exists('BaseOAuth')) :
+
+class BaseOAuth {
+	const VERSION = '0.1.0';
+
+	/**
+	 * BASE API v1
+	 * https://gist.github.com/baseinc/9634675
+	 *
+	 * @var string
+	 */
+	public static $host = 'https://api.thebase.in/1/';
+	/**
+	 * OAuth
+	 * GET /1/oauth/authorize - 認可コードを取得
+	 * POST /1/oauth/token - 認可コードからアクセストークンを取得
+	 * POST /1/oauth/token - リフレッシュトークンからアクセストークンを取得
+	 */
+	const OAUTH_AUTHORIZE = 'oauth/authorize';
+	const OAUTH_TOKEN = 'oauth/token';
+	/**
+	 * Users
+	 * GET /1/users/me - ユーザー情報を取得
+	 */
+	const USERS_ME = 'users/me';
+	/**
+	 * Items
+	 * GET /1/items - 商品情報の一覧を取得
+	 * GET /1/items/detail/:item_id - 商品情報を取得
+	 * POST /1/items/add - 商品情報を登録
+	 * POST /1/items/edit - 商品情報を更新
+	 * POST /1/items/delete - 商品情報を削除
+	 * POST /1/items/add_image - 商品情報の画像を登録
+	 * POST /1/items/delete_image - 商品情報の画像を削除
+	 * POST /1/items/edit_stock - 商品情報の在庫数を更新
+	 */
+	const ITEMS = 'items';
+	/**
+	 * Orders
+	 *
+	 * GET /1/orders - 注文情報の一覧を取得
+	 * GET /1/orders/detail/:unique_key - 注文情報を取得
+	 * POST /1/orders/edit_status - 注文情報のステータスを更新
+	 */
+	const ORDERS = 'orders';
+	/**
+	 * Savings
+	 *
+	 * GET /1/savings - 引き出し申請情報の一覧を取得
+	 */
+	const SAVINGS = 'savings';
+
+	/* Contains the last API call. */
+	public $url = null;
+	/* Contains the last HTTP status code returned. */
+	public $http_code = null;
+	/* Response by API. */
+	public $response = array();
+
+
+	public function __construct( $config=array() ) {
+	}
+
+	public function request( $method, $url, $params=array(), $useauth=true, $multipart=false ) {
+		return true;
+	}
+
+	/**
+	 * GET /1/oauth/authorize - 認可コードを取得
+	 *
+	 * @param $client_id
+	 * @param $redirect_uri
+	 * @param null $scope
+	 * @param null $state
+	 *
+	 * @return string
+	 * @internal param $params
+	 */
+	public function getAuthorize( $client_id, $redirect_uri, $scope=null, $state=null ) {
+		/**
+		 * response_type code (必須)
+		 * client_id     クライアントID (必須)
+		 * redirect_uri  登録したコールバックURL (必須)
+		 * scope         スコープをスペース区切りで指定 (任意 デフォルト: read_users)
+		 * state         リダイレクト先URLにそのまま返すパラメーター (任意)
+		 */
+		$params = array(
+			'response_type' => 'code',
+			'client_id'     => $client_id,
+			'redirect_uri'  => $redirect_uri,
+		);
+		$scope and ($params['scope'] = $scope);
+		$state and ($params['state'] = $state);
+
+		return $this->url = self::build_url(self::OAUTH_AUTHORIZE,$params);
+	}
+
+
+	/**
+	 * POST /1/oauth/token - 認可コードからアクセストークンを取得
+	 * memo
+	 * リクエストするたびに違うaccess_token,refresh_tokenが取得できる
+	 * 認可コードの有効期限は？←「アプリを認証する」をポチポチするたびに発行される
+	 * Error: bool(false)
+	 * POST /1/oauth/token - リフレッシュトークンからアクセストークンを取得
+	 *
+	 * @param $grant_type
+	 * @param $client_id
+	 * @param $client_secret
+	 * @param $code_or_refresh_token
+	 * @param $redirect_uri
+	 *
+	 * @return array|mixed|string
+	 */
+	public function getToken($grant_type, $client_id, $client_secret, $code_or_refresh_token, $redirect_uri) {
+		/**
+		 * grant_type           authorization_code / refresh_token (必須)
+		 * client_id            クライアントID (必須)
+		 * client_secret        クライアントシークレット (必須)
+		 * code/refresh_token   認可コード / リフレッシュトークン (必須)
+		 * redirect_uri         登録したコールバックURL (必須)
+		 */
+		$params = array(
+			'grant_type'     => $grant_type,
+			'client_id'      => $client_id,
+			'client_secret'  => $client_secret,
+			($grant_type==='authorization_code')?'code':'refresh_token' => $code_or_refresh_token,
+			'redirect_uri'   => $redirect_uri
+		);
+		$this->url = self::$host . self::OAUTH_TOKEN;
+
+		$response = $this->_post($this->url, $params);
+		/**
+		 * access_token - APIにアクセスするために必要なトークン。有効期限は1時間。
+		 * token_type - bearer
+		 * expires_in - アクセストークンの有効期限
+		 * refresh_token - アクセストークンを再発行するために必要なトークン。有効期限は30日。
+		 */
+		if ($response)
+			$response = json_decode($response);
+
+		return $response;
+	}
+
+	public function users() {
+	}
+	public function items() {
+	}
+	public function orders() {
+	}
+	public function savings() {
+	}
+
+	/**
+	 * $urlに $paramsをPOSTする
+	 * Set $http_code
+	 * Set $response
+	 *
+	 * @param $url
+	 * @param $params
+	 *
+	 * @return string
+	 */
+	private function _post($url, $params) {
+		//FIXME CURLに変更する
+		$headers = array(
+			'Content-Type: application/x-www-form-urlencoded',
+		);
+		$request_options = array(
+			'http' => array(
+				'method'  => 'POST',
+				'content' => http_build_query($params),
+				'header'  => implode("\r\n", $headers),
+			),
+		);
+		$context = stream_context_create($request_options);
+		$this->response = file_get_contents($url, false, $context);
+		$this->http_code = ($this->response)? 200 : 400;//FIXME CURLに変更
+		return $this->response;
+	}
+
+	private function _get() {
+	}
+
+	private function _request() {
+		//cURLを初期化して使用可能にする
+		$curl=curl_init();
+		//オプションにURLを設定する
+		curl_setopt($curl,CURLOPT_URL,$authorize_uri);
+		//CA証明書の検証をしない
+		curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,false);
+		//文字列で結果を返させる
+		curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
+		// User-Agent
+		curl_setopt($curl, CURLOPT_USERAGENT, $user_agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36');
+		// referrer
+		curl_setopt($curl, CURLOPT_REFERER, $ref='http://google.com/');
+		//URLにアクセスし、結果を表示させる
+		//$html = curl_exec($curl);
+		//cURLのリソースを解放する
+		curl_close($curl);
+		//var_dump($html);
+
+	}
+
+
+	/**
+	 * Make an HTTP request
+	 *
+	 * @param string $url
+	 * @param string $method
+	 * @param null $postfields
+	 * @return OAuthRequest results
+	 */
+	function http($url, $method, $postfields = NULL) {
+		$this->http_info = array();
+		$ci = curl_init();
+		/* Curl settings */
+		curl_setopt($ci, CURLOPT_USERAGENT, $this->useragent);
+		curl_setopt($ci, CURLOPT_CONNECTTIMEOUT, $this->connecttimeout);
+		curl_setopt($ci, CURLOPT_TIMEOUT, $this->timeout);
+		curl_setopt($ci, CURLOPT_RETURNTRANSFER, TRUE);
+		curl_setopt($ci, CURLOPT_SSL_VERIFYPEER, $this->ssl_verifypeer);
+		curl_setopt($ci, CURLOPT_HEADERFUNCTION, array($this, 'getHeader'));
+		curl_setopt($ci, CURLOPT_HEADER, FALSE);
+
+		$this->setHeaders( $ci );
+
+		switch ($method) {
+			case 'POST':
+				curl_setopt($ci, CURLOPT_POST, TRUE);
+				if (!empty($postfields)) {
+					curl_setopt($ci, CURLOPT_POSTFIELDS, $postfields);
+				}
+				break;
+			case 'DELETE':
+				curl_setopt($ci, CURLOPT_CUSTOMREQUEST, 'DELETE');
+				if (!empty($postfields)) {
+					$url = "{$url}?{$postfields}";
+				}
+		}
+
+		if ($this->proxy_host) {
+			curl_setopt($ci, CURLOPT_PROXY, $this->proxy_host);
+			curl_setopt($ci, CURLOPT_PROXYPORT, $this->proxy_port);
+			curl_setopt($ci, CURLOPT_PROXYUSERPWD, $this->proxy_userpwd);
+		}
+		//curl_setopt($ci, CURLINFO_HEADER_OUT,true); var_dump($postfields);
+		$this->setUrl( $ci, $url );
+		$response = curl_exec($ci);
+		$error = curl_error($ci);
+		$this->http_code = curl_getinfo($ci, CURLINFO_HTTP_CODE);
+		$this->http_info = array_merge($this->http_info, curl_getinfo($ci));
+		curl_close ($ci);
+		return $response;
+	}
+
+
+
+	/**
+	 * Get Request build
+	 *
+	 * @param $url
+	 * @param Array $params
+	 *
+	 * @return string
+	 */
+	public static function build_url($url, $params=null) {
+
+		if (! is_array($params))
+			return self::$host . $url;
+
+		if (phpversion() >= 5.4) {
+			$str = http_build_query($params, null, "&", PHP_QUERY_RFC3986);
+		} else {
+			$str = http_build_query($params, null, "&");
+			$str = str_replace('+','%20', $str);
+		}
+
+		return self::$host . $url . '?' . $str;
+	}
+
+}
+
+endif;
---
+++ install.php
@@ -0,0 +1,196 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: K.Sasaki
+ * Date: 2014/05/22
+ * Time: 13:21
+ */
+
+
+error_reporting(E_ALL);
+//TODO client_id, client_secretがない場合、BASE API developer登録を行ってもらい（済の場合省略可）、client_id, client_secretを入力させる。
+
+// $_GET initialize
+empty($_GET['step']) and $_GET['step'] = null;
+empty($_GET['oauth']) and $_GET['oauth'] = null;
+empty($_GET['reset_account']) and $_GET['reset_account'] = null;
+empty($_GET['installing']) and $_GET['installing'] = null;
+//var_dump($_GET);
+
+$install_uri = admin_url('admin.php?page=base_to_wp_install');
+$redirect_uri = $install_uri . '&installing=1&step=5&oauth=1';//FIXME installingいらない
+
+//FIXME DEVELOP define $REDIRECT_URI_DEV and BASE_HOST_DEV
+include BASE_TO_WP_ABSPATH . '/config.php';
+$redirect_uri = REDIRECT_URI_DEV;
+\OAuth\BaseOAuth::$host = BASE_HOST_DEV;
+
+var_dump(
+get_option('base_to_wp_client_key'),
+get_option('base_to_wp_client_secret'),
+get_option('base_to_wp_install_stage'),
+
+get_option('base_to_wp_access_token'),
+get_option('base_to_wp_refresh_token'),
+get_option('base_to_wp_request_oauth'),
+get_option('base_to_wp_account_activated'),
+//初期設定
+get_option("base_to_wp_hogehoge"),
+get_option('base_to_wp_piyopiyo')
+);
+
+
+if ($_POST) {
+	// client key 保存
+	if ($_GET['step'] == '3') {
+		if ($_POST['base_to_wp_client_key'] != '' && $_POST['base_to_wp_client_secret'] != '') {
+			update_option('base_to_wp_client_key', $_POST['base_to_wp_client_key']);
+			update_option('base_to_wp_client_secret', $_POST['base_to_wp_client_secret']);
+		} else {
+			$_GET['step'] = '2';
+		}
+	}
+}
+
+//BASE API 認可コードのリダイレクト
+if ($_GET['oauth']=='1') {
+	$_GET['step'] = '5';
+}
+
+// Step 保存
+if ($_GET['step']) {
+	update_option('base_to_wp_install_stage', $_GET['step']);
+}
+
+// Stage 呼び出し
+$stage = get_option('base_to_wp_install_stage');
+if ($stage == '') {
+	add_option('base_to_wp_install_stage', '1');
+	$stage = '1';
+}
+
+//var_dump($stage);
+?>
+
+
+<div class="wrap">
+	<h2>BASE To WordPress Setup</h2>
+
+	<?php if (isset($_GET['reset_account']) && $_GET['step']==='1') : ?>
+		<div id="message" class="updated fade">
+			<p>BASE APIクライアント登録が完了している場合は同じクライアントが使用できます。「Step1」をスキップして次のステップへ進んでください。</p>
+		</div>
+	<?php endif; ?>
+
+	<div style="width: 70%;">
+
+	<?php if ($stage == '1') : ?>
+
+		<h3><span style="font-size: 150%">Step 1 :</span> このブログをBASE API クライアントとして登録してください</h3>
+		<p>セキュリティ上の理由から、BASE To WordPress はあなたのBASEアカウントを認証し、アクセスするためにBASE APIのOAuthのプロトコルを使用しています。それを動作させるためには、BASE API のクライアントとしてあなたのサイトを登録する必要があります。BASE.inにアクセスし、クライアント登録するには、以下のボタンをクリックしてください。</p>
+		<p style="text-align:center"><a class="button" href="http://apps.thebase.in/" target="_blank">BASE API クライアント登録</a></p>
+		<p>あなたはBASE APPに到着すると、あなたは下の図のようなフォームが表示されます。「新しいアプリケーションを登録」をクリックしてください。いくつかのフィールドを入力することになります。</p>
+		<p style="text-align:center"><img src="<?php echo plugin_dir_url(__FILE__) ?>/images/wizard_1.png" alt="BASE APP Form" /></p>
+		<p>あなたは、アイコンをアップロードする必要はありません。それは完全に任意だし、誰もそれを見ていないんです。</p>
+		<ol style="list-style:decimal; margin: 0 25px 15px 25px;">
+			<li><strong>[名前]</strong>フィールドにブログの名前を入力します。</li>
+			<li><strong>[説明]</strong>ボックスにあなたのブログの簡単な説明を入れてください。アプリケーションを識別するいくつかの単語。</li>
+			<li>あなたのブログのURLをフィールドに入力します。（ex. <em>http://www.luck2.co.jp</em>）</li>
+			<li>コールバックURLフィールドに<strong><code><?php echo $redirect_uri; ?></code></strong>を貼り付けます。</li>
+			<li>[保存]ボタンを押して、 [次へ]表示されたページで[設定]タブをクリックします。</li>
+			<li>読み書きするアクセスオプションを設定します。</li>
+			<li>アプリケーションの設定を保存し、セットアップを続行するためにこのページに戻って来てください。</li>
+		</ol>
+
+		<p style="text-align:right"><a class="button" href="<?php echo $install_uri . '&step=2'; ?>">次へ進む</a></p>
+	<?php endif;?>
+
+	<?php if ($stage == '2') : ?>
+		<h3><span style="font-size: 150%">Step 2 :</span> BASE APP で取得した Client Key と Client Secret を入力</h3>
+		<p>BASE APIとのインターフェイスにワードプレスを有効にするには、キーを入力する必要があります。あなたは<a href="http://thebase.in/apps/" target="_blank">このBASE APPページ</a>を訪問して、前のステップで登録されたアプリケーションを選択することでそれらを見つけることができます。</p>
+		<p style="text-align:center"><img src="<?php echo plugin_dir_url(__FILE__) ?>/images/wizard_2.png" alt="Finding the Application Keys" /></p>
+		<p><strong>Client Key</strong> と <strong>Client Secret</strong> を入力してください。</p>
+		<form method="post" action="<?php echo $install_uri . '&step=3'; ?>">
+			<table class="form-table">
+				<tr valign="top">
+					<th scope="row"><label for="base_to_wp_client_key">Client Key</label></th>
+					<td><input name="base_to_wp_client_key" type="text" id="base_to_wp_client_key" value="<?php echo get_option('base_to_wp_client_key'); ?>" class="regular-text" /></td>
+				</tr>
+				<tr valign="top">
+					<th scope="row"><label for="base_to_wp_client_secret">Client Secret</label></th>
+					<td><input name="base_to_wp_client_secret" type="text" id="base_to_wp_client_secret" value="<?php echo get_option('base_to_wp_client_secret'); ?>" class="regular-text" /></td>
+				</tr>
+			</table>
+			<input type="hidden" name="save_app_keys" value="yes" />
+			<p class="submit" style="text-align:right"><input type="submit" name="Submit" class="button-primary" value="保存して次へ" /></p>
+		</form>
+	<?php endif ?>
+
+	<?php
+	if ($stage == '3') :
+
+		//認可コードを取得
+		$BaseOAuth = new \OAuth\BaseOAuth();
+		$authorize_uri = $BaseOAuth->getAuthorize(
+			$client_id = get_option('base_to_wp_client_key'),
+			$redirect_uri,
+			$scope = 'read_users read_items read_orders',
+			$stage = 'hogehoge'
+		);
+	?>
+		<h3><span style="font-size: 150%">Step 4 :</span> BASE アカウントの認証</h3>
+		<p>もう少し！今すぐあなたのBASEアカウントにアクセスできるようにあなたのブログを承認する必要があります。</p>
+		<p>下のボタンをクリックすると、api.thebasedev.in へ移動します。あなたがすでにログインしている場合は、あなたのブログを認可するためのオプションが表示されます。「アプリを認証する」ボタンを押して、自動でここへ戻って来ます。</p>
+		<p style="text-align:center">
+			<a href="<?php echo $authorize_uri; ?>" class="button">BASE API おーそりぼたん</a>
+		</p>
+	<?php endif; ?>
+
+	<?php if ($stage == '5') :
+
+//		var_dump($_GET['code']);
+
+		//認可コードからaccess_token,refresh_tokenを取得して保存する
+		$BaseOAuth = new \OAuth\BaseOAuth();
+		$response = $BaseOAuth->getToken(
+			$grant_type = 'authorization_code',
+			$client_id = get_option('base_to_wp_client_key'),
+			$client_secret = get_option('base_to_wp_client_secret'),
+			$code = $_GET['code'],//FIXME Sanitize
+			$redirect_uri
+		);
+
+		var_dump($response);
+
+		if ($BaseOAuth->http_code == 200) :
+			//Update WP options
+			update_option('base_to_wp_access_token', $response->access_token);
+			update_option('base_to_wp_refresh_token', $response->refresh_token);
+			delete_option('base_to_wp_request_oauth');
+			update_option('base_to_wp_account_activated', '1');
+			//初期設定
+			update_option("base_to_wp_hogehoge", '0');
+			update_option('base_to_wp_piyopiyo', array(
+				1 => 'bar',
+				2 => get_bloginfo('name'),
+				3 => 'from:foo'
+			));
+		?>
+			<h3><span style="font-size: 150%">Step 5 :</span> おめでとう！すべてが完了しました！</h3>
+			<p>あなたのBASEアカウントにアクセスするためにこのブログを承認しました。あなたのBASEショップはワードプレスと連携することができます。</p>
+			<p style="text-align:right">
+				<a class="button" href="<?php echo admin_url('admin.php?page=base_to_wp'); ?>">完了</a>
+			</p>
+		<?php else : ?>
+			<h3>何かが間違っていた...</h3>
+			<p>BASE To WordPressは、あなたのBASEアカウントを認証することができませんでした。</p>
+		<?php endif; ?>
+	<?php endif; ?>
+
+		<br />
+		<small><a href="<?php echo $install_uri . '&reset_account=1&step=1'; ?>">[再セットアップ]</a></small>
+	</div>
+</div>
+
+
+
